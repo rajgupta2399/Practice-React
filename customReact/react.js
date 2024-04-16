209219809925d26686d2f1d3f55c17881de80196
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-04-17 00:06:50 +0530

    custom

---
+++ customReact/react.js
@@ -0,0 +1,42 @@
+// const { Children } = require("react");
+
+function customRender(reactElement, container) {
+
+    /*
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.Children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.Children;
+
+    for(const prop in reactElement.props){
+        if(prop == "Children") continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    container.appendChild(domElement)
+
+
+
+
+}
+
+const reactElement = {
+  type: "a", // which type of element have to create
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  Children: "Click Me to Google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
